0e4a5e4c1c7fa5a1fdc56f9d48e637765cb269da
Sergio Visinoni <sergio.visinoni@schibsted.com.mx>
2014-10-21 09:26:53 -0500

    Add default static IP configuration and override mechanism
    We don't want to use DHCP(?) for the time being, meaning that we
    need a way to deploy the VM with a static IP address without having
    to always change the source code.
    
    Add the default IP (Dev environment) in config.ml, and allow these
    values to be overridden through environment variables.

--- _mirage/config.ml
+++ _mirage/config.ml
@@ -4,6 +4,13 @@ open Mirage
    MODE=crunch (or nothing): use static filesystem via crunch
    MODE=fat: use FAT and block device (run ./make-fat-images.sh)
  *)
+
+(* These are the default values we use when building on our dev
+   environment *)
+let def_address = "10.2.0.103"
+let def_netmask = "255.255.255.0"
+let def_gateway = "10.2.0.1"
+
 let mode =
   try match String.lowercase (Unix.getenv "FS") with
     | "fat" -> `Fat
@@ -31,10 +38,30 @@ let dhcp =
     | _  -> true
   with Not_found -> false
 
+let ip =
+  try match Sys.getenv "IP" with
+    | "" -> def_address
+    | x  -> x
+  with Not_found -> def_address
+
+let nm =
+  try match Sys.getenv "NETMASK" with
+    | "" -> def_netmask
+    | x  -> x
+  with Not_found -> def_netmask
+
+let gw =
+  try match Sys.getenv "GW" with
+    | "" -> def_gateway
+    | x  -> x
+  with Not_found -> def_gateway
+
+let ipv4_config =  { address = Ipaddr.V4.of_string_exn ip; netmask = Ipaddr.V4.of_string_exn nm; gateways = [Ipaddr.V4.of_string_exn gw] }
+
 let stack console =
   match net, dhcp with
   | `Direct, true  -> direct_stackv4_with_dhcp console tap0
-  | `Direct, false -> direct_stackv4_with_default_ipv4 console tap0
+  | `Direct, false -> direct_stackv4_with_static_ipv4 console tap0 ipv4_config
   | `Socket, _     -> socket_stackv4 console [Ipaddr.V4.any]
 
 let server =
